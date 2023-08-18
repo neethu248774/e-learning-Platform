d00daf89534ae5ad5ad36d10bd7b1e15d66a3a76
neethu248774 <248774@ust.com>
2023-08-18 09:52:51 +0530

    first commit

---
+++ back-end/src/main/java/com/vacademy/vacademybackend/VAcademyBackendApplication.java
@@ -0,0 +1,12 @@
+package com.vacademy.vacademybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VAcademyBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VAcademyBackendApplication.class, args);
+        System.out.println("Hellooo World");
+    }
+}
